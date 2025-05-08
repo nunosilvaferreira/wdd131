086a45df6c7885792ad1152390d43be4319e51a5
nunosilvaferreira <nferreira@byupathway.edu>
2025-05-08 15:18:39 +0100

    w01.1.1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year for copyright
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
